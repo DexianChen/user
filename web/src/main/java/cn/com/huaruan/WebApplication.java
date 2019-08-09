4d704022c7ff7b612cf468f3d2635fe1bd3d0845
chendexian <4>
2019-08-09 08:48:29 +0800

    fix(): 修改springboot版本为2.x

--- web/src/main/java/cn/com/huaruan/WebApplication.java
+++ web/src/main/java/cn/com/huaruan/WebApplication.java
@@ -2,13 +2,14 @@ package cn.com.huaruan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = {"cn.com.huaruan.mapper", "cn.com.huaruan.service", "cn.com.huaruan.controller"})
+@ComponentScan(basePackages = {"cn.com.huaruan.mapper", "cn.com.huaruan.service", "cn.com.huaruan.controller"
+        , "cn.com.huaruan.conf"})
 public class WebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class);
